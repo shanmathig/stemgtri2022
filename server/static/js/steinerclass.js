fe80ad88f71bce9a3f46f529d1941b9dbccab17d
Gunasekaran <sgunasekaran32@ctisl.gtri.org>
2022-06-16 14:32:05 -0400

    Starter code

--- server/static/js/steinerclass.js
+++ server/static/js/steinerclass.js
@@ -18,17 +18,18 @@ class Point {
         this.y = y;
     }
 
-    drawPoint(strokeWeight, strokeColor, coordinateGridReference, type) {
-        if (strokeColor === undefined) {
-            strokeColor = 'black';
+    drawPoint(strokeWeight, fillColor, coordinateGridReference, type) {
+        if (fillColor === undefined) {
+            fillColor = 'black';
         }
         if (type !== undefined) {
-            noStroke();
+            noFill();
             text(type, this.x + 30, this.y + 20);
         }
-        stroke(strokeColor).strokeWeight(strokeWeight);
-        circle(this.x, this.y, 40);
-        stroke('black');
+        fill(fillColor);
+        ellipse(this.x, this.y, 20, 20);
+        fill('black');
+
     }
 }
 
@@ -62,3 +63,62 @@ class Node extends Point {
   
 
 //Now for the whole thing!
+
+class Grid {
+    constructor(windowWidth, windowHeight, numColumns, numRows) {
+        this.windowWidth = windowWidth;
+        this.windowHeight = windowHeight;
+        this.numColumns = numColumns;
+        this.numRows = numRows;
+        
+        this.nodePoints = {};
+        this.points = [];
+    }
+    drawPoints() {
+        for (var i = 0; i < this.points.length; i++) {
+            for (var j = 0; j < this.points[i].length; j++) {
+                var pointObject = this.points[i][j];
+                pointObject.drawPoint(5, undefined, this);
+            }   
+        }
+    }
+    
+    getIdAndLabel(node) {
+        var id = node.substring(node.lastIndexOf("<") + 1, node.lastIndexOf(">"));
+        var label = node.replace(/<.*>/, '').replace(/[.*]/, '');
+        return {
+            id, label
+        }
+    }
+
+    getNodePoint(id) {
+        return this.nodePoints[id];
+    }
+
+    addNodePoint(id, nodePoint) {
+        this.nodePoints[id] = nodePoint;
+    }
+
+    loadNodePoints(nodeData) {
+        for (const [key, value] of Object.entries(nodeData)) {
+            var relativeX = value.coordinates[0];
+            var relativeY = value.coordinates[1];
+            var {realX, realY} = this.calculateRealCoords(relativeX, relativeY);
+            var {id, label} = this.getIdAndLabel(key);
+            var rootNodeStatus = value.root_node;
+
+            print(this.points[relativeX][relativeY])
+            var newNodePoint = new NodePoint(
+                relativeX, // relativeX
+                relativeY, // relativeY
+                realX, // x
+                realY, // y
+                id, // id
+                label, // label
+                rootNodeStatus // rootNode
+            );
+            this.points[relativeX][relativeY] = newNodePoint;
+            this.addNodePoint(id, newNodePoint);
+        }
+    }
+}

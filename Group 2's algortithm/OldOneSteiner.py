29eeb6b83c40ee2c64c433bd2ed8e0976ad094d6
Brown <cbrown602@ctisl.gtri.org>
2022-07-13 13:28:45 -0400

    Wirelengths passed in data

--- Group 2's algortithm/OldOneSteiner.py
+++ Group 2's algortithm/OldOneSteiner.py
@@ -1,11 +1,14 @@
 from MST import MST
 from Result import resulting_Nodes
 def OldSteiner(Nodes):
+    print("starting")
     Edges = MST(Nodes)
+    
     #Compute current wire length
     Wire_Length_Initial = 0
     for i in range(len(Edges)):
         Wire_Length_Initial = Wire_Length_Initial + abs(Nodes[Edges[i][0]][0]-Nodes[Edges[i][1]][0]) + abs(Nodes[Edges[i][0]][1]-Nodes[Edges[i][1]][1])
+    
     #Compute all possible additional points
     Temp_Positions = []
     Newnodes = []
@@ -22,7 +25,7 @@ def OldSteiner(Nodes):
                 if Temp_Positions not in Nodes:
                     Newnodes.append(Temp_Positions)
             Temp_Positions = []
-    #Recalculate all possible MSLs with all extra possible points from the point pool we calculated (1 at a time)
+    #Recalculate all possible MSTs with all extra possible points from the point pool we calculated (1 at a time)
     Temp_Positions = Nodes
     Wire_Length_Best=Wire_Length_Initial
     Best_Node = []
@@ -35,6 +38,7 @@ def OldSteiner(Nodes):
             if(Wire_Length_Final<Wire_Length_Best):
                 Wire_Length_Best=Wire_Length_Final
                 Best_Node = Newnodes[i] 
+                print(Newnodes[i])
             Temp_Positions.pop()
         if (Best_Node == []):
             resulting_Nodes(Temp_Positions)
--- Group 2's algortithm/app.py
+++ Group 2's algortithm/app.py
@@ -5,19 +5,22 @@ from OneSteiner import OneSteiner
 from MST import MST
 from OldOneSteiner import OldSteiner
 from Randomfunc import Randomfun
+from OneSteiner import Eval
 app = Flask(__name__)
 
 @app.route("/", methods=['get'])
 def __init__():
     Parsed = parser()
     Onesteined = OldSteiner(Parsed[:])
-    data = { "f":[], "i":[], "msti":[], "msts":[] }
+    data = { "f":[], "i":[], "msti":[], "msts":[], "Wirelengths": []}
     data["i"].append(Parsed)
     data["f"].append(Onesteined)
     data["msti"].append(MST(Parsed))
+    data["Wirelengths"].append(Eval(Parsed))
     Nodes_Standin = parser()
     for i in range(len(Onesteined)):
         Nodes_Standin.append(Onesteined[i][:])
+        data["Wirelengths"].append(Eval(Nodes_Standin))
         data["msts"].append(MST(Nodes_Standin))
     print(data)
     return render_template("steiner.html", data=data)
@@ -26,14 +29,16 @@ def __init__():
 def Slow():
     Parsed = parser()
     Onesteined = OneSteiner(Parsed)
-    data = { "f":[], "i":[], "msti":[], "msts":[] }
+    data = { "f":[], "i":[], "msti":[], "msts":[], "Wirelengths": []}
     data["i"].append(Parsed)
     data["f"].append(Onesteined)
     data["msti"].append(MST(Parsed))
+    data["Wirelengths"].append(Eval(Parsed))
     Nodes_Standin = parser()
     for i in range(len(Onesteined)):
         Nodes_Standin.append(Onesteined[i][:])
         data["msts"].append(MST(Nodes_Standin))
+        data["Wirelengths"].append(Eval(Nodes_Standin))
     print(data)
     return render_template("steiner.html", data=data)
 
@@ -42,13 +47,14 @@ def radnom():
     Parsed = Randomfun()
     Nodes_Standin = Parsed[:]
     Onesteined = OldSteiner(Parsed[:])
-    data = { "f":[], "i":[], "msti":[], "msts":[] }
+    data = { "f":[], "i":[], "msti":[], "msts":[], "Wirelengths": []}
     data["i"].append(Parsed)
     data["f"].append(Onesteined)
     data["msti"].append(MST(Parsed))
-    
+    data["Wirelengths"].append(Eval(Parsed))
     for i in range(len(Onesteined)):
         Nodes_Standin.append(Onesteined[i][:])
         data["msts"].append(MST(Nodes_Standin)[:])
+        data["Wirelengths"].append(Eval(Nodes_Standin))
     print(data)
     return render_template("steiner.html", data=data)
